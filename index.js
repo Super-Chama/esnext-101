d0aef8981156460fe1f30cc8ea6b55ca93d84ffe
Amil Waduwawara <amilwaduwawara@orangehrm.us.com>
2024-11-26 21:53:53 +0530

    Excercise-03 - DONE

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { exercise01 } from "./promises/exercise-01.js";
 import { exercise02 } from "./promises/exercise-02.js";
-// import { exercise03 } from "./promises/exercise-03.js";
-// import { exercise04 } from "./promises/exercise-04.js";
-// import { exercise05 } from "./promises/exercise-05.js";
+import { exercise03 } from "./promises/exercise-03.js";
+import { exercise04 } from "./promises/exercise-04.js";
+import { exercise05 } from "./promises/exercise-05.js";
 
 console.log(await exercise01());
 console.log(await exercise02());
+console.log(await exercise03());
